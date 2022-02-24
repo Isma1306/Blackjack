c654ff3054cf6d4cab9fd7a4c8bdb4ec8102b4d7
Isma1306 <81107025+Isma1306@users.noreply.github.com>
2022-02-24 17:18:43 +0100

    Reorganized the js
    I made it more object oriented and easier to understand.
    
    I have to decide and implement the point system.

--- index.js
+++ index.js
@@ -1,141 +1,151 @@
-//----------------------------- deck model ----------------------//
-// I have based the structure of my model in a model made by Colt Steel in his
-// javaScript codecamp that I have done in Udemy.
-const deckModel = {
-  //----------------------------- properties ------------------//
-  suits: ["spades", "clubs", "diamonds", "hearts"],
-  ranks: ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"],
-  deck: [],
-  playerCards: [],
-  dealerCards: [],
-  playerHandValue: 0,
-  dealerHandValue: 0,
-
-  //--------------------------- methods ------------------------//
-  createDeck(number = 1) {
+//----------------------------- deck class -------------------------//
+// I created a class deck so I can have the methods inside the class and
+// the flexibility to use this class even in another game card.
+class Deck {
+  constructor(numOfDecks) {
+    //--------------------------- deck properties ---------------------//
+    this.numOfdecks = numOfDecks;
+    this.suits = ["spades", "clubs", "diamonds", "hearts"];
+    this.ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    this.createDeck(numOfDecks);
+    this.shuffle();
+  }
+  //--------------------------- deck methods ------------------------//
+  createDeck(numOfDecks = 1) {
     // create a deck with n decks inside (normally is played between 1 and 8)
-    const { ranks, suits, deck } = this;
-    for (let i = 0; i < number; i++) {
+    const { ranks, suits } = this;
+    let cards = [];
+    for (let i = 0; i < numOfDecks; i++) {
       for (let suit of suits) {
         for (let rank of ranks) {
-          deck.push({ rank, suit });
+          cards.push({ rank, suit });
         }
       }
+      this.cards = cards;
     }
     console.log("Deck created");
-  },
+  }
   shuffle(times = 10) {
     // shuffle the deck, making shuffling look like real shuffling is quite complicated
-    const { deck } = this;
-    for (let i = 0; i < deck.length * times; i++) {
-      let index = Math.floor(Math.random() * deck.length); // If I find some fancy function I will change this
-      deck.push(deck[index]);
-      deck.splice(index, 1);
+    const { cards } = this;
+    for (let i = 0; i < cards.length * times; i++) {
+      let index = Math.floor(Math.random() * cards.length); // If I find some fancy function I will change this
+      cards.push(cards[index]);
+      cards.splice(index, 1);
     }
     console.log("Deck shuffled");
-  },
-  dealPlayerCards(number = 1) {
-    const { deck, playerCards } = this;
-    for (let i = 0; i < number; i++) {
-      let card = deck.pop();
-      playerCards.push(card);
-    }
-  },
-
-  dealDealerCards(number = 1) {
-    const { deck, dealerCards } = this;
-    for (let i = 0; i < number; i++) {
-      let card = deck.pop();
-      dealerCards.push(card);
-    }
-  },
-
-  countPlayerHand() {
-    const { playerCards } = this;
-    this.playerHandValue = 0;
-    playerCards.forEach((card) => {
-      let value = card.rank;
-      if (value === "K" || value === "J" || value === "Q") {
-        this.playerHandValue = this.playerHandValue + 10;
-      } else if (!isNaN(value)) {
-        this.playerHandValue = this.playerHandValue + value;
-      }
-    });
-    playerCards.forEach((card) => {
-      let value = card.rank;
-      if (value === "A" && this.playerHandValue + 11 <= 21) {
-        this.playerHandValue = this.playerHandValue + 11;
-      } else if (value === "A" && this.playerHandValue + 11 > 21) {
-        this.playerHandValue = this.playerHandValue + 1;
-      }
-    });
-    console.log(this.playerHandValue);
-  },
-
-  countDealerHand() {
-    const { dealerCards } = this;
-    this.dealerHandValue = 0;
-    dealerCards.forEach((card) => {
-      let value = card.rank;
-      if (value === "K" || value === "J" || value === "Q") {
-        this.dealerHandValue = this.dealerHandValue + 10;
-      } else if (!isNaN(value)) {
-        this.dealerHandValue = this.dealerHandValue + value;
-      }
-    });
-    dealerCards.forEach((card) => {
-      let value = card.rank;
-      if (value === "A" && this.dealerHandValue + 11 <= 21) {
-        this.dealerHandValue = this.dealerHandValue + 11;
-      } else if (value === "A" && this.dealerHandValue + 11 > 21) {
-        this.dealerHandValue = this.dealerHandValue + 1;
-      }
-    });
-    console.log(this.dealerHandValue);
-  },
-};
-
-deckModel.createDeck();
-deckModel.shuffle();
-deckModel.dealPlayerCards(2);
-deckModel.countPlayerHand();
-deckModel.dealDealerCards(2);
-deckModel.countDealerHand();
-console.log(deckModel.playerCards);
-deckModel.playerHandValue;
-console.log(deckModel.dealerCards);
-deckModel.dealerHandValue;
-
-function checkhand() {
-  let { playerHandValue } = deckModel;
-  if (playerHandValue > 21) {
-    return console.log("you lost");
-  } else if (playerHandValue <= 21) {
-    return console.log("keep playing");
   }
 }
-function hit() {
-  deckModel.dealPlayerCards();
-  deckModel.countPlayerHand();
-  checkhand();
+
+//----------------------------- player class ----------------------//
+class Player {
+  constructor(name = "Player1", points = 200) {
+    //----------------------------- player propieties -----------------//
+    this.name = name;
+    this.points = points;
+    this.hand = [];
+  }
+
+  //----------------------------- player methods --------------------//
+  drawCards(number = 1, deck) {
+    const { hand } = this;
+    for (let i = 0; i < number; i++) {
+      let card = deck.cards.pop();
+      hand.push(card);
+    }
+    this.countHand();
+  }
+  discardHand() {
+    const { hand } = this;
+    for (let i = hand.length; i > 0; i--) {
+      hand.pop();
+    }
+  }
+
+  countHand() {
+    const { hand } = this;
+    let handValue = 0;
+    hand.forEach(function (card) {
+      let rank = card.rank;
+      if (rank === "K" || rank === "J" || rank === "Q") {
+        handValue = handValue + 10;
+      } else if (!isNaN(rank)) {
+        handValue = handValue + rank;
+      }
+    });
+    hand.forEach(function (card) {
+      let rank = card.rank;
+      if (rank === "A" && handValue + 11 <= 21) {
+        handValue = handValue + 11;
+      } else if (rank === "A" && handValue + 11 > 21) {
+        handValue = handValue + 1;
+      }
+    });
+    this.handValue = handValue;
+    console.log(handValue);
+  }
+
+  checkBusted() {
+    const { handValue } = this;
+    if (handValue > 21) {
+      return console.log("you went busted!");
+    } else if (handValue <= 21) {
+      return console.log("keep playing, what could go wrong?");
+    }
+  }
+  checkBlackjack() {
+    const { handValue, hand } = this;
+    let blackjack;
+    if (hand.length === 2 && handValue === 21) {
+      blackjack = true;
+    } else {
+      blackjack = false;
+    }
+    this.blackjack = blackjack;
+    return blackjack;
+  }
 }
+
+//-----------------------------  game setup --------------------//
+const mainDeck = new Deck(1);
+const player1 = new Player();
+const cpu = new Player("cpu");
+
+//-----------------------------  game actions --------------------//
+function start() {
+  player1.drawCards(2, mainDeck);
+  player1.checkBlackjack();
+  cpu.drawCards(1, mainDeck);
+}
+
+function restart() {
+  player1.discardHand();
+  cpu.discardHand();
+  mainDeck.createDeck(mainDeck.numOfDecks);
+  mainDeck.shuffle();
+  start();
+}
+
+function hit(player) {
+  player.drawCards(1, mainDeck);
+  player.checkBusted();
+}
+
 function stand() {
-  let { playerHandValue, dealerHandValue } = deckModel;
-  while (deckModel.dealerHandValue < 17) {
-    deckModel.dealDealerCards();
-    deckModel.countDealerHand();
+  while (cpu.handValue < 17) {
+    cpu.drawCards(1, mainDeck);
   }
-  if (playerHandValue < dealerHandValue && dealerHandValue < 21) {
+  cpu.checkBlackjack();
+  if (player1.blackjack === true && cpu.blackjack === false) {
+    console.log("you won with a Blackjack!");
+  } else if ((player1.blackjack === true && cpu.blackjack === true) || player1.handValue === cpu.handValue) {
+    console.log("stand-off");
+  } else if (player1.handValue < cpu.handValue && cpu.handValue <= 21) {
     console.log("you lost");
   } else {
     console.log("you win");
   }
 }
 
-// const game = {
-//   playerPoints: 100,
-//   bet: 10,
-//   playerHandValue: 0,
-//   dealerHandValue: 0,
-
-// };
+//-----------------------------  game start --------------------//
+start();
