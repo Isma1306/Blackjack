7eed1e31c86e9855b0bcf1813e2ead25c8fa55b8
Isma1306 <81107025+Isma1306@users.noreply.github.com>
2022-02-28 16:29:43 +0100

    Finished with the GUI
    fixed the blackjack bug and finished with the GUI
    
    the game is fully functional now I have to organize the code better and test everything.

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ class Deck {
       }
       this.cards = cards;
     }
-    console.log("Deck created");
   }
   shuffle(times = 10) {
     // shuffle the deck, making shuffling look like real shuffling is quite complicated
@@ -34,7 +33,6 @@ class Deck {
       cards.push(cards[index]);
       cards.splice(index, 1);
     }
-    console.log("Deck shuffled");
   }
 }
 
@@ -53,8 +51,8 @@ class Player {
     for (let i = 0; i < number; i++) {
       let card = deck.cards.pop();
       hand.push(card);
-      // testing!!
-      renderCard(name, hand, card);
+
+      gui.renderCard(name, hand, card);
     }
     this.countHand();
   }
@@ -85,8 +83,7 @@ class Player {
       }
     });
     this.handValue = handValue;
-    console.log(handValue);
-    renderHandValue(this.name, handValue);
+    gui.renderHandValue(this.name, handValue);
   }
 
   isBlackjack() {
@@ -118,29 +115,26 @@ class Game {
     const player = new Player(this.playerName);
     this.player = player;
     player.drawCards(2, mainDeck);
-    player.isBlackjack();
     cpu.drawCards(1, mainDeck);
+    player.isBlackjack();
     if (this.player.blackjack) {
       this.stand();
     } else {
-      btnOn("hitButton");
-      btnOn("standButton");
+      gui.btnOn("hitBtn");
+      gui.btnOn("standBtn");
     }
   }
   restart() {
     this.player.discardHand();
     this.cpu.discardHand();
-    destroyCards();
+    gui.destroyCards();
     const mainDeck = new Deck(this.numOfDecks);
     this.mainDeck = mainDeck;
     this.player.drawCards(2, this.mainDeck);
     this.player.isBlackjack();
     this.cpu.drawCards(1, this.mainDeck);
     if (this.player.blackjack) {
-      stand();
-    } else {
-      btnOn("hitButton");
-      btnOn("standButton");
+      this.stand();
     }
   }
   hit() {
@@ -148,142 +142,136 @@ class Game {
     this.isBust(this.player);
   }
   stand() {
-    btnOff("hitButton");
-    btnOff("standButton");
     while (this.cpu.handValue < 17) {
       this.cpu.drawCards(1, this.mainDeck);
     }
-    renderPoints();
     this.cpu.isBlackjack();
     if (this.player.blackjack === true && this.cpu.blackjack === false) {
-      renderRestart(0, this.player.blackjack); // win with blackjack
-      this.player.points = this.player.points + 20 * 1.5;
+      gui.renderRestart("win", this.player.blackjack); // win with blackjack
+      this.player.points = this.player.points + 30;
     } else if ((this.player.blackjack === true && this.cpu.blackjack === true) || this.player.handValue === this.cpu.handValue) {
-      renderRestart(1, this.player.blackjack); // stand off
+      gui.renderRestart("tie", this.player.blackjack); // stand off
     } else if (this.player.handValue < this.cpu.handValue && this.cpu.handValue <= 21) {
-      renderRestart(2, this.player.blackjack); // lost
+      gui.renderRestart("lose", this.player.blackjack); // lost
       this.player.points = this.player.points - 20;
     } else {
-      renderRestart(0, this.player.blackjack); // win
+      gui.renderRestart("win", this.player.blackjack); // win
       this.player.points = this.player.points + 20;
     }
+    gui.renderPoints();
   }
   isBust(player) {
     if (player.handValue > 21) {
       player.points = this.player.points - 20;
-      renderPoints();
-      btnOff("hitButton");
-      btnOff("standButton");
-      renderRestart(3, this.player.blackjack); // bust
-    } else if (player.handValue <= 21) {
-      return console.log("keep playing, what could go wrong?");
+      gui.renderPoints();
+      gui.renderRestart("bust", this.player.blackjack); // bust
     }
   }
 }
 
-//----------------------------- controls? class? ----------------------//
-
-$("#hitButton").on("click", function () {
-  game.hit();
-});
-
-$("#standButton").on("click", function () {
-  game.stand();
-  renderPoints();
-});
-$("#startForm").on("submit", function (event) {
-  event.preventDefault();
-  let name = $("#startForm").find("#name").val();
-  let numberOfDecks = $("#startForm").find("#numberOfDecks").val();
-  $("#startMenu").hide(200);
-  game = new Game(name, numberOfDecks);
-  game.start();
-  renderPoints();
-});
-
 //----------------------------- render GUI ----------------------//
-
-function renderCard(name, hand, card) {
-  let padding = hand.indexOf(card) * 30;
-  let divId = "";
-  if (name === "cpu") {
-    divId = "cpu";
-  } else {
-    divId = "player";
-  }
-  $(`<div class="cardSpace"><div class="card">
-<div class="cardText"><h3>${card.rank}<br>${card.suit}</h3></div>
-<div class="cardSuit">${card.suit}</div></div>
-</div>`)
-    .css("padding-left", padding)
-    .appendTo(`#${divId} .cardHolder`);
-  $(".card:contains('♦')").addClass("red");
-  $(".card:contains('♥')").addClass("red");
-}
-
-function renderHandValue(name, handValue) {
-  let divId;
-  if (name === "cpu") {
-    divId = "#cpuHandValue";
-  } else {
-    divId = "#playerHandValue";
-  }
-  $(divId).text(handValue);
-}
-
-function renderPoints() {
-  console.log(game.player.points);
-  $("#points").text(game.player.points + " Points left!");
-}
-
-function destroyCards() {
-  $(".cardSpace").remove();
-}
-
-function btnOn(id) {
-  $("#" + id).prop("disabled", false);
-}
-
-function btnOff(id) {
-  $("#" + id).prop("disabled", true);
-}
-
-function renderRestart(result, blackjack) {
-  $("#startMenu").empty();
-  if (result === 0 && blackjack) {
-    $("#startMenu").append(`<div class="restartMenu">
-  <h1>${game.playerName} you Won!</h1> <h2>With Blackjack!</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 1 && blackjack) {
-    $("#startMenu").append(`<div class="restartMenu">
-  <h1>Stand-Off</h1> <h2>Two blackjacks at the same time!?</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 1 && blackjack) {
-    $("#startMenu").append(`<div class="restartMenu">
-  <h1>Stand-Off</h1> <h2>Two blackjacks at the same time!?</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 1 && !blackjack) {
-    $("#startMenu").append(`<div class="restartMenu">
-    <h1>Stand-Off</h1> <h2>At least you didn't lose</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 0) {
-    $("#startMenu").append(`<div class="restartMenu">
-      <h1>${game.playerName} you Won!</h1> <h2>If I keep losing they will kick me out!</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 2) {
-    $("#startMenu").append(`<div class="restartMenu">
-      <h1>${game.playerName} you Lost!</h1> <h2>Lucky me!</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
-  } else if (result === 3) {
-    $("#startMenu").append(`<div class="restartMenu">
-      <h1>${game.playerName} went bust!</h1> <h2>I told you to stand!</h2> <button id="restartBtn" class="menuBtn">Deal</button></div>`);
+class Gui {
+  constructor() {}
+  
+  renderCard(name, hand, card) {
+    let padding = hand.indexOf(card) * 30;
+    let divId = "";
+    if (name === "cpu") {
+      divId = "cpu";
+    } else {
+      divId = "player";
+    }
+    $(`<div class="cardSpace"><div class="card">
+      <div class="cardText"><h3>${card.rank}<br>${card.suit}</h3></div>
+      <div class="cardSuit">${card.suit}</div></div></div>`)
+      .css("padding-left", padding)
+      .appendTo(`#${divId} .cardHolder`);
+    $(".card:contains('♦')").addClass("red");
+    $(".card:contains('♥')").addClass("red");
   }
 
-  $("#startMenu").show();
+  renderHandValue(name, handValue) {
+    let divId;
+    if (name === "cpu") {
+      divId = "#cpuHandValue";
+    } else {
+      divId = "#playerHandValue";
+    }
+    $(divId).text(handValue);
+  }
 
-  console.log("renderRestart");
+  renderPoints() {
+    $("#points").text(game.player.points + " Points left!");
+  }
 
-  $("#restartBtn").on("click", function () {
-    $("#startMenu").hide(200);
-    game.restart();
+  destroyCards() {
+    $(".cardSpace").remove();
+  }
+
+  btnOn(id) {
+    $("#" + id).prop("disabled", false);
+  }
+
+  btnOff(id) {
+    $("#" + id).prop("disabled", true);
+  }
+
+  renderRestart(result, blackjack) {
+    $("#menu").empty();
+    $("#restartBtn").show();
+    $("#hitBtn").hide();
+    $("#standBtn").hide();
+    if (result === "win" && blackjack) {
+      $("#menu").append(`<h1>${game.playerName} won!</h1> <h2>With Blackjack! Lucky me!</h2>`);
+    } else if (result === "tie" && blackjack) {
+      $("#menu").append(`<h1>Stand-Off</h1> <h2>Two blackjacks at the same time!?</h2>`);
+    } else if (result === "tie" && !blackjack) {
+      $("#menu").append(`<h1>Stand-Off</h1> <h2>At least you didn't lose.</h2>`);
+    } else if (result === "win") {
+      $("#menu").append(`<h1>${game.playerName} won!</h1> <h2>Deep Blue will play the next hand.</h2>`);
+    } else if (result === "lose") {
+      $("#menu").append(`<h1>${game.playerName} lost!</h1> <h2>Against five lines of code.</h2>`);
+    } else if (result === "bust") {
+      $("#menu").append(`<h1>${game.playerName} went bust!</h1> <h2>The next one is a ${game.player.hand[game.player.hand.length - 1].rank}, Oh! is it too late?</h2>`);
+    }
+    $("#menu").show(200);
+
+    $("#restartBtn").on("click", function () {
+      $("#menu").hide(200);
+      $("#restartBtn").hide();
+      $("#hitBtn").show();
+      $("#standBtn").show();
+      game.restart();
+    });
+  }
+}
+
+function init() {
+  $("#hitBtn").on("click", function () {
+    game.hit();
+  });
+
+  $("#standBtn").on("click", function () {
+    game.stand();
+    gui.renderPoints();
+  });
+
+  $("#startForm").on("submit", function (event) {
+    event.preventDefault();
+    let name = $("#startForm").find("#name").val();
+    let numberOfDecks = $("#startForm").find("#numberOfDecks").val();
+    $("#menu").hide(300);
+    game = new Game(name, numberOfDecks);
+    gui = new Gui();
+    game.start();
+    gui.renderPoints();
   });
 }
 
-//----------------------------- game start ----------------------//
+init();
 
-// const circleType = new CircleType(document.getElementById('rules'));
-// circleType.radius(200).dir(-1);
+//----------------------------- circleType code -----------------//
+const rulesBj = new CircleType(document.getElementById("rulesBj"));
+rulesBj.radius(400).dir(-1);
+const rulesDealer = new CircleType(document.getElementById("rulesDealer"));
+rulesDealer.radius(500).dir(-1);
